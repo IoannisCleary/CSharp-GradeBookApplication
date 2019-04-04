62f24e5b9e0063a4cf5b3ae98ac2ec6f3fb538b3
Ioannis Cleary <ioannis.cleary@ceridian.com>
2019-04-04 09:41:31 +0100

    Missing import fix

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,5 @@
 ﻿
-
+using System;
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
@@ -13,7 +13,7 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count<5){
-                throw new InvalidOperactionException();
+                throw new InvalidOperationException();
             }
             return 'F';
         }
